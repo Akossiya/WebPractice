c8d2f3defa858ce96b826a0e2c8ebe78c14cac8e
Asik <akossiyabek@gmail.com>
2020-03-28 12:58:57 +0600

    adding files

--- week9/shop-back/shopback/api/urls.py
+++ week9/shop-back/shopback/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from api.views import product_list
 from api.views import product_detail
+from api.views import category_list
+from api.views import category_detail
+from api.views import category_product
 
 urlpatterns = [
     path('products/', product_list),
     path('products/<int:id>', product_detail),
+    path('categories/', category_list),
+    path('categories/<int:id>', category_detail()),
+    path('categories/<int:id>/products/',category_product)
 ]
--- week9/shop-back/shopback/api/views.py
+++ week9/shop-back/shopback/api/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse, JsonResponse
 from api.models import Product
+from api.models import Category
 from django.http import Http404
 
 
@@ -18,4 +19,19 @@ def product_detail(request, id):
         product = Product.objects.get(id=id)
     except Product.DoesNotExist as e:
         raise Http404
+    return JsonResponse(product.to_json())
+
+def category_list(request):
+    categories = Category.objects.all()
+    categories_json = [category.to_json() for category in categories]
+    return JsonResponse(categories_json, safe=False)
+def category_detail(request, id):
+    try:
+        category = Category.objects.get(id=id)
+    except Category.DoesNotExist as e:
+        raise Http404
+    return JsonResponse(category.to_json())
+def category_product(request, id):
+    category = Category.objects.get(id=id)
+    product = Product.objects.all()
     return JsonResponse(product.to_json())
